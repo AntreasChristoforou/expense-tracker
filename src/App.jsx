e132f93c6e5c975addba823c02313fcaa570a742
AntreasChristoforou <christoforou.a8@gmail.com>
2026-09-21 16:07:34 +0300

    List of expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 
+const CATEGORIES = ["Food", "Transport", "Bills", "Shopping", "Fun", "Health"];
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState(
+    [
+      {id : 1, name : "water", amount: 40, category: "Bills"},
+      {id : 2, name : "Diner", amount: 30, category: "Food" },
+    ]
+  )
 
   return (
-    
+    <div>
+      <ul>
+        {
+          expenses.map(expense => <li key={expense.id}>
+            {expense.name} - €{expense.amount.toFixed(2)} - {expense.category}
+          </li>)
+        }
+      </ul>
+    </div>
   )
 }
 
